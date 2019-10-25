371b01e8aec62d2bbfda212123437325897dfde4
Adepurnomo <ade.purnomo50@gmail.com>
2019-10-25 20:57:51 +0700

    Update XOA_Installer.sh

--- XOA_Installer.sh
+++ XOA_Installer.sh
@@ -1,62 +1,64 @@
 #!/bin/bash
 ##
 clear
-echo "Basic scripts"
+kuning=$(tput setaf 3)
 hijau=$(tput setaf 2)
-echo "${hijau}###########################################"
-echo "${hijau}Please run this scripts on SU !"
+echo "${kuning}###########################################"
+echo "${kuning}Please run this scripts on SU !"
 sudo su -
-echo "${hijau}Min spec for cluster, backup, continues"
-echo "${hijau}replication (DC-DR),load balancing etc,"
-echo "${hijau}for XenServer/CitrixHyprrvisor/XCP-NG  "
-echo "${hijau}Like XCP, use guest tools from citrix xen"
-echo "${hijau}4vCPU, 16GB RAM, NIC 10G or Bounded"
-echo "${hijau}###########################################"
+echo "${kuning}Min spec for cluster, backup, continues"
+echo "${kuning}replication (DC-DR),load balancing etc,"
+echo "${kuning}for XenServer/CitrixHyprrvisor/XCP-NG  "
+echo "${kuning}Like XCP, use guest tools from citrix xen"
+echo "${kuning}4vCPU, 16GB RAM, NIC 10G or Bounded"
+echo "${kuning}###########################################"
 tuned-adm profile network-throughput
 hostnamectl set-hostname XOA
-echo "${hijau}..................................."
-echo "${hijau}Working...."
-echo "${hijau}Please wait.."
+echo "${kuning}..................................."
+echo "${kuning}Working...."
+echo "${kuning}Please wait.."
 /bin/yum install epel-release curl -y > /dev/null 2>&1
 #yum update -y > /dev/null 2>&1
-# add repo node js
-echo "${hijau}..................................."
+#add repo node js
+mkdir -p /opt/temp
+curl -o /opt/temp/spinner.sh https://raw.githubusercontent.com/tlatsas/bash-spinner/master/spinner.sh >> /dev/null 2>&1
+chmod a+x /opt/temp/spinner.sh
+
+echo "${kuning}..................................."
 echo "add repo nodeJS v8.xx..."
 curl -s -L https://rpm.nodesource.com/setup_8.x | bash - > /dev/null 2>&1
 curl -s -o /etc/yum.repos.d/yarn.repo https://dl.yarnpkg.com/rpm/yarn.repo > /dev/null 2>&1
 # Node
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "install nodeJS...."
 echo "Please wait......"
 sleep 1
 /bin/yum install nodejs -y  > /dev/null 2>&1
 # install yarn package
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "Install yarn package...."
 sleep 2
 /bin/yum install yarn -y > /dev/null 2>&1
 # install lib vhd tools
 sleep 1
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "Install vhd tools...."
 /bin/rpm -ivh https://forensics.cert.org/cert-forensics-tools-release-el7.rpm > /dev/null 2>&1
 /bin/sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/cert-forensics-tools.repo > /dev/null 2>&1
 /bin/yum --enablerepo=forensics install -y libvhdi-tools > /dev/null 2>&1
 echo "Install tool 4 xoa..."
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 sleep 1
-/bin/yum install gcc gcc-c++ make openssl-devel redis libpng-devel python36 git nfs-utils -y > /dev/null 2>&1
-# install ssl
-echo "${hijau}..................................."
+/bin/yum install gcc gcc-c++ make openssl-devel redis libpng-devel python36 git nfs-utils -y >> /dev/null 2>&1
 echo "Install Open SSL.."
 /bin/yum install mod_ssl -y > /dev/null 2>&1
 # enable service redis etc 
 echo "enable redis server..."
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 /bin/systemctl enable redis && /bin/systemctl start redis
 /bin/systemctl enable rpcbind && /bin/systemctl start rpcbind 
 echo " Tools succes install"
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 node=$(node -v) 
 npm=$(npm -v)
 yarn=$(yarn --version)
@@ -64,23 +66,30 @@ echo "Node js version $node"
 echo "NPM version $npm"
 echo "Yarn package version $yarn"
 sleep 10
-echo "${hijau}..................................."
-echo "clone xoa from source"
-echo "${hijau}..................................."
+echo "${kuning}..................................."
+echo "clone xoa from source ---_____----_____-----"
 cd /opt/
-/usr/bin/git clone https://github.com/vatesfr/xen-orchestra 
+/usr/bin/git clone https://github.com/vatesfr/xen-orchestra >> /dev/null 2>&1
 # allow config restore
 sed -i 's/< 5/> 0/g' /opt/xen-orchestra/packages/xo-web/src/xo-app/settings/config/index.js
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "Build your XOA ..."
-echo "${hijau}..................................."
+echo "${kuning}..................................."
+echo "${kuning}4 look activity build-XOA, open new screen then"
+echo "${kuning} paste tail -f 500 /opt/temp/yarn-build-xoa.log"
+source "/opt/temp/spinner.sh"
+start_spinner ' Build XOA, please wait (take several minute...'
+sleep 1
 cd /opt/xen-orchestra
-/usr/bin/yarn
-/usr/bin/yarn build
+/usr/bin/yarn >> /dev/null 2>&1
+/usr/bin/yarn build >> /opt/temp/yarn-build-xoa.log
+cd /opt/temp
+stop_spinner $?
+
 # configure xoa
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "--------------Configure xoa----------------"
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 sleep 5
 cd /opt/xen-orchestra/packages/xo-server
 \cp sample.config.toml .xo-server.toml
@@ -93,17 +102,17 @@ chmod a+x /opt/xen-orchestra/packages/xo-server/.xo-server.toml
 /bin/sed -i "s|# key = './key.pem'|key = '/opt/cert/key-selfsigned.pem'|" .xo-server.toml
 # create node
 echo "Create node ..."
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 sleep 2
 mkdir -p /usr/local/lib/node_modules/
 /bin/ln -s /opt/xen-orchestra/packages/xo-server-* /usr/local/lib/node_modules/
 /bin/rm -rf /etc/systemd/system/xo-server.service
 # Cow BANNER
 echo "attach kebo banner :v"
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 sleep 2
 cd /root/
-/bin/git clone https://github.com/Adepurnomo/banner.git
+/bin/git clone https://github.com/Adepurnomo/banner.git >> /dev/null 2>&1
 \cp /root/banner/issue.net /etc
 /bin/chmod a+x /etc/issue.net
 cd /etc/ssh/ 	
@@ -111,9 +120,9 @@ cd /etc/ssh/
 /bin/chmod a+x /etc/ssh/sshd_config
 /bin/rm -rf /root/banner
 #Create service for xoa
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 echo "........ write service on systemd ........."
-echo "${hijau}..................................."
+echo "${kuning}..................................."
 /bin/cat << EOF >> /etc/systemd/system/xo-server.service
 # Systemd service for XO-Server.
 [Unit]
@@ -130,36 +139,40 @@ SyslogIdentifier=xo-server
 WantedBy=multi-user.target
 EOF
 
-echo "${hijau}..................................."
-echo "     Configure self sign ssl for xoa       "
-echo "${hijau}..................................."
+echo "${kuning}..................................."
+echo " Configure self sign ssl for xoa..........."
+echo "${kuning}..................................."
 sleep 2
 mkdir /opt/cert
 /bin/chmod 700 /opt/cert
-echo "Generate OpenSSL"
-echo "Please wait......"
-echo "${hijau}..................................."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /opt/cert/key-selfsigned.pem -out /opt/cert/cert-selfsigned.pem -subj "/C=Id/ST=DKI Jakarta/L=Jakarta/O=Ade Purnomo/OU=IT Department/CN=Port of Tanjung Priok"
-openssl dhparam -out /opt/cert/dhparam.pem 2048 > /dev/null 2>&1
-/bin/cat /opt/cert/dhparam.pem | tee -a /opt/cert/cert-selfsigned.pem
 
-echo "Allow port 80 on firewalld"
+source "/opt/temp/spinner.sh"
+start_spinner 'Configure self sign ssl for xoa, please wait'
+sleep 1
+cd ~
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /opt/cert/key-selfsigned.pem -out /opt/cert/cert-selfsigned.pem -subj "/C=Id/ST=DKI Jakarta/L=Jakarta/O=Ade Purnomo/OU=IT Department/CN=Port of Tanjung Priok" >> /dev/null 2>&1
+openssl dhparam -out /opt/cert/dhparam.pem 2048 >> /dev/null 2>&1
+/bin/cat /opt/cert/dhparam.pem | tee -a /opt/cert/cert-selfsigned.pem >> /dev/null 2>&1
+cd /opt/temp
+stop_spinner $?
+
+echo "white list 80 on firewalld"
 /bin/firewall-cmd --zone=public --add-port=80/tcp --permanent 
-echo "Allow port 443 on firewalld"
+echo "white list 443 on firewalld"
 /bin/firewall-cmd --zone=public --add-port=443/tcp --permanent
-/bin/firewall-cmd --reload
+/bin/firewall-cmd --reload /dev/null 2>&1
 
 /bin/systemctl daemon-reload > /dev/null 2>&1
-/bin/systemctl enable xo-server.service && /bin/systemctl start xo-server > /dev/null 2>&1
+/bin/systemctl enable xo-server.service && /bin/systemctl start xo-server >> /dev/null 2>&1
 
 sleep 2
-echo "${hijau}+++++++++++++++++++++++++++"
-echo "${hijau}========="DONE"============"
-echo "${hijau}+++++++++++++++++++++++++++"
+echo "${kuning}+++++++++++++++++++++++++++"
+echo "${kuning}========="DONE"============"
+echo "${kuning}+++++++++++++++++++++++++++"
 host=$(hostname -I)
 echo "and then acces https://$host"
 echo "username : admin@admin.net"
 echo "password : admin"
 #echo "follow my ig @nextorchestra / @pauziah_collection"
-echo "${hijau}------------------------------------------------"
+echo "${kuning}------------------------------------------------"
 /bin/systemctl restart sshd.service > /dev/null 2>&1
