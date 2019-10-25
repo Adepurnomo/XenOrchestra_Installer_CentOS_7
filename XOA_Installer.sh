b75c5dd72c81fe14816c3876dd7e7b6777de55ea
Adepurnomo <ade.purnomo50@gmail.com>
2019-10-26 00:18:09 +0700

    Update XOA_Installer.sh

--- XOA_Installer.sh
+++ XOA_Installer.sh
@@ -3,15 +3,15 @@
 clear
 kuning=$(tput setaf 3)
 hijau=$(tput setaf 2)
-echo "${hijau}###########################################"
-echo "${hijau}Please run this scripts on SU !"
+echo "${hijau}#################################################"
+echo "${hijau}        Please run this scripts on SU !"
 sudo su -
-echo "${hijau}Min spec for cluster, backup, continues"
-echo "${hijau}replication (DC-DR),load balancing etc,"
-echo "${hijau}for XenServer/CitrixHyprrvisor/XCP-NG  "
-echo "${hijau}Like XCP, use guest tools from citrix xen"
-echo "${hijau}4vCPU, 16GB RAM, NIC 10G or Bounded"
-echo "${hijau}###########################################"
+echo "${hijau}    Min spec for cluster, backup, continues"
+echo "${hijau}     replication (DC-DR),load balancing etc,"
+echo "${hijau}    for XenServer/CitrixHyprrvisor/XCP-NG  "
+echo "${hijau}    Like XCP, use guest tools from citrix xen"
+echo "${hijau}      4vCPU, 16GB RAM, NIC 10G or Bounded"
+echo "${hijau}#################################################"
 tuned-adm profile network-throughput
 hostnamectl set-hostname XOA
 echo "${kuning}------------------------------------------------"
