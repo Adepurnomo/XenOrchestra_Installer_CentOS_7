4b8426dd6e10d7494535e9af8b862876f28a2d32
Adepurnomo <ade.purnomo50@gmail.com>
2019-10-29 16:49:54 +0700

    Update XOA_Installer.sh

--- XOA_Installer.sh
+++ XOA_Installer.sh
@@ -4,19 +4,19 @@ clear
 kuning=$(tput setaf 3)
 hijau=$(tput setaf 2)
 echo "${hijau}#################################################"
-echo "${hijau}        Please run this scripts on SU !"
+echo "${hijau}        Please run this scripts on SU !          "
 sudo su -
-echo "${hijau}    Min spec for cluster, backup, continues"
-echo "${hijau}     replication (DC-DR),load balancing etc,"
-echo "${hijau}    for XenServer/CitrixHyprrvisor/XCP-NG  "
-echo "${hijau}    Like XCP, use guest tools from citrix xen"
-echo "${hijau}      4vCPU, 16GB RAM, NIC 10G or Bounded"
+echo "${hijau}    Min spec for cluster, backup, continues      "
+echo "${hijau}     replication (DC-DR),load balancing etc,     "
+echo "${hijau}    for XenServer/CitrixHyprrvisor/XCP-NG        "
+echo "${hijau}    Like XCP, use guest tools from citrix xen    "
+echo "${hijau}      4vCPU, 16GB RAM, NIC 10G or Bounded        "
 echo "${hijau}#################################################"
 tuned-adm profile network-throughput
 hostnamectl set-hostname XOA
 echo "${kuning}------------------------------------------------"
-echo "${kuning}Working...."
-echo "${kuning}Please wait.."
+echo "${kuning}Working....                                     "
+echo "${kuning}Please wait...                                  "
 /bin/yum install epel-release curl -y > /dev/null 2>&1
 #yum update -y > /dev/null 2>&1
 
@@ -25,19 +25,19 @@ sudo mkdir -p /opt/temp
 sudo curl -o /opt/temp/spinner.sh https://raw.githubusercontent.com/tlatsas/bash-spinner/master/spinner.sh >> /dev/null 2>&1
 sudo chmod a+x /opt/temp/spinner.sh
 echo "${kuning}------------------------------------------------"
-echo "add repo nodeJS v8.xx..."
+echo "add repo nodeJS v8.xx...                                 "
 curl -s -L https://rpm.nodesource.com/setup_8.x | bash - > /dev/null 2>&1
 curl -s -o /etc/yum.repos.d/yarn.repo https://dl.yarnpkg.com/rpm/yarn.repo > /dev/null 2>&1
 
 # Node
 echo "${kuning}------------------------------------------------"
-echo "install nodeJS...."
-echo "Please wait......"
+echo "install nodeJS....                                       "
+echo "Please wait......                                        "
 sleep 1
 /bin/yum install nodejs -y  > /dev/null 2>&1
 # install yarn package
 echo "${kuning}------------------------------------------------"
-echo "Install yarn package...."
+echo "Install yarn package....                                 "
 sleep 2
 /bin/yum install yarn -y > /dev/null 2>&1
 
@@ -56,7 +56,7 @@ echo "Install Open SSL.."
 /bin/yum install mod_ssl -y > /dev/null 2>&1
 
 # enable service redis etc 
-echo "enable redis server..."
+echo "enable redis server...                                   "
 echo "${kuning}------------------------------------------------"
 /bin/systemctl enable redis > /dev/null 2>&1
 /bin/systemctl start redis > /dev/null 2>&1
@@ -67,19 +67,19 @@ echo "${kuning}------------------------------------------------"
 node=$(node -v) 
 npm=$(npm -v)
 yarn=$(yarn --version)
-echo "Node js version $node"
-echo "NPM version $npm"
-echo "Yarn package version $yarn"
+echo "Node js version $node                                    "
+echo "NPM version $npm                                         "
+echo "Yarn package version $yarn                               "
 sleep 10
 echo "${kuning}------------------------------------------------"
-echo "clone xoa from source ---_____----_____-----"
+echo "clone xoa from source ---_____----_____-----             "
 cd /opt/
 /usr/bin/git clone https://github.com/vatesfr/xen-orchestra >> /dev/null 2>&1
 
 # allow config restore
 sed -i 's/< 5/> 0/g' /opt/xen-orchestra/packages/xo-web/src/xo-app/settings/config/index.js
 echo "${kuning}------------------------------------------------"
-echo "Build your XOA..."
+echo "Build your XOA...                                        "
 echo "${kuning}------------------------------------------------"
 echo "${kuning}4 look activity first & last XOA, open new screen then"
 echo "${kuning}------------------------------------------------"
@@ -88,7 +88,7 @@ echo "${kuning}------------------------------------------------"
 source "/opt/temp/spinner.sh"
 start_spinner 'First yarn 4 xoa..please wait (take several minute..'
 sleep 1
-#----____-----_____------_____------______------_______------___
+
 cd ~
 cd /opt/xen-orchestra
 /usr/bin/yarn >> /opt/temp/yarn-xoa.log 
@@ -104,7 +104,7 @@ cd /opt/temp
 stop_spinner $?
 
 # configure xoa
-echo "--------------------Configure xoa-----------------------"
+echo "--------------------Configure XOA-----------------------"
 sleep 5
 cd /opt/xen-orchestra/packages/xo-server
 \cp sample.config.toml .xo-server.toml
@@ -124,7 +124,7 @@ sleep 2
 mkdir -p /usr/local/lib/node_modules/
 /bin/ln -s /opt/xen-orchestra/packages/xo-server-* /usr/local/lib/node_modules/
 /bin/rm -rf /etc/systemd/system/xo-server.service
-# Cow BANNER
+# Banner
 echo "attach banner :v"
 echo "${kuning}------------------------------------------------"
 sleep 2
@@ -136,9 +136,8 @@ cd /etc/ssh/
 /bin/sed -i "s|#Banner none|Banner /etc/issue.net|" sshd_config
 /bin/chmod a+x /etc/ssh/sshd_config
 /bin/rm -rf /root/banner
-
 #Create service for xoa
-echo "~write service on systemd~"
+echo "${kuning}        ~write service on systemd~              "
 /bin/cat << EOF >> /etc/systemd/system/xo-server.service
 
 # Systemd service for XO-Server.
@@ -157,7 +156,7 @@ WantedBy=multi-user.target
 EOF
 
 echo "${kuning}------------------------------------------------"
-echo "Configure self sign ssl for xoa..........."
+echo "Configure self sign ssl for xoa...........               "
 echo "${kuning}------------------------------------------------"
 sleep 2
 mkdir /opt/cert
@@ -190,12 +189,12 @@ rm -rf /opt/temp
 
 sleep 2
 echo "${kuning}------------------------------------------------"
-echo "${kuning}                      DONE                      " 
+echo "${kuning}                     DONE                       " 
 echo "${kuning}------------------------------------------------"
 host=$(hostname -I)
-echo "and then acces https://$host"
-echo "username : admin@admin.net"
-echo "password : admin"
+echo "and then acces https://$host                             "
+echo "username : admin@admin.net                               "
+echo "password : admin                                         "
 echo "${kuning}------------------------------------------------"
 echo "    for update your xoa, choose dir xoa installer        "
 echo "cd '/opt/xen-orchestra' & run command 'git pull --ff-only'"
